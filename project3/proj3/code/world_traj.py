214588b14b17bc72bb91c61ad38f51ebcf884767
JoshuaErne <josherne@seas.upenn.edu>
2023-04-28 00:45:01 -0400

    Updated Code

--- project3/proj3/code/world_traj.py
+++ project3/proj3/code/world_traj.py
@@ -1,10 +1,10 @@
 import numpy as np
 
+from scipy.special import factorial
 from scipy.sparse import  csc_matrix
 from scipy.sparse.linalg import spsolve
-from scipy.sparse import SparseEfficiencyWarning
-from scipy.sparse import SparseWarning
 from scipy.sparse import lil_matrix
+import matplotlib.pyplot as plt
 
 import sys
 sys.path.append('/home/josh/Desktop/meam620/project3/proj3/code/')
@@ -13,7 +13,6 @@ from .graph_search import graph_search
 
 ###################################################################################################
 ###################################### Related Works can be found in Readme  ######################
-
 def perpendicular_distance(p, a, b):
 
     direction_vector = np.divide(b - a, np.linalg.norm(b - a))
@@ -24,7 +23,7 @@ def perpendicular_distance(p, a, b):
 
     return np.hypot(h, np.linalg.norm(c))
 
-def RDP(trajectory, perp_threshold = 0.05):
+def RDP(trajectory, perp_threshold = 0.1):
     start             = 0
     end               = len(trajectory)-1
     maximum_Distance  = 0 
@@ -73,11 +72,11 @@ class WorldTraj(object):
         # planning. In the previous project these were provided to you; now you
         # must chose them for yourself. Your may try these default values, but
         # you should experiment with them!
-        # self.resolution = np.array([0.2, 0.2, 0.2])
-        # self.margin = 0.2
-
         self.resolution = np.array([0.25, 0.25, 0.25])
         self.margin = 0.5
+
+        # self.resolution = np.array([0.2, 0.2, 0.2])
+        # self.margin = 0.55
 
         # You must store the dense path returned from your Dijkstra or AStar
         # graph search algorithm as an object member. You will need it for
@@ -89,11 +88,12 @@ class WorldTraj(object):
         # too close together. Store these waypoints as a cla8ss member; you will
         # need it for debugging and it will be used when plotting results.
         self.points = np.zeros((1,3)) # shape=(n_pts,3)
-        # print(self.path.shape)    
+        print(self.path.shape)    
         self.points =  RDP(self.path)
+        # self.points = self.path
         # self.points_new = RDP(self.path)
 
-        # print(self.points.shape)
+        print(self.points.shape)
         # print()
         # print(self.points_new.shape)
         # Finally, you must compute a trajectory through the waypoints similar
@@ -103,7 +103,8 @@ class WorldTraj(object):
         # semester.
 
         # STUDENT CODE HERE
-        self.speed  = 2.8
+        # self.speed  = 2.8
+        self.speed = 4
 
         self.points_len = len(self.points)
         
@@ -121,47 +122,61 @@ class WorldTraj(object):
             self.distance_lst.append(distance)
 
             if distance < 2.5:
-                self.speed = 2.8
+                self.speed = 2.3
             elif distance > 2.5 and distance < 3.0:
-                self.speed = 3.65
+                self.speed = 4
             elif distance > 3:
-                self.speed = 3.7
+                self.speed = 6
 
             self.segment_velocity.append(self.speed * I)
-            self.segment_duration.append(distance/self.speed)
+            self.segment_duration.append(np.clip(distance/self.speed, 1, np.inf))
             self.segment_startime.append(self.segment_startime[idx] + (distance/self.speed))
 
+
+        #Matrix Solution for 8th Order Polynomial
+        order = 8
         n = self.points.shape[0] -1
-        B = lil_matrix((8 * n, 3))
-        A = lil_matrix((8 * n, 8 * n))
-        self.segment_duration = np.clip(np.array(self.segment_duration),0.25, np.inf)
+
+        #Assemble the constraint Matrix and solve AX=b where b is 3D
+        B = np.zeros((order * n, 3))
+        A = np.zeros((order * n, order * n))
+
+        #End points boundary conditions Intialization
+        A[0,6] = 1
+        A[1,5] = 2
+        A[2,4] = 6
 
         for cnt in range(n):
-            B[8 * cnt + 3] = self.points[cnt]
-            B[8 * cnt + 4] = self.points[cnt + 1]
-            t_each = self.segment_duration[cnt+1]
-            Mat = self.generate_M(t_each)
+            cnt_up = cnt * order
+            #Generate nth Order derivative matrix
+            Mat    = self.generate_M(self.segment_duration[cnt+1])
 
-            A[[0, 1, 2], [6, 5, 4]] = [1, 2, 6]
+            #Position Boundary Constraints
+            A[cnt_up +3, cnt_up: cnt_up + 8] = np.array([0,0,0,0,0,0,0,1])
+            
+            #Assign Continuity Constraints with unknown coefficients to end of segments
+            #If we have two segments tn and t(n+1), the derivative at the end of segment n equals the derivative at the start of segment n+1
             if cnt != (n-1):
-                A[5 + 8 * cnt, 14 + 8 * cnt] = -1
-                A[6 + 8 * cnt, 13 + 8 * cnt] = -2
-                A[7 + 8 * cnt, 12 + 8 * cnt] = -6
-                A[8 + 8 * cnt, 11 + 8 * cnt] = -24
-                A[9 + 8 * cnt, 10 + 8 * cnt] = -120
-                A[10 + 8 * cnt, 9 + 8 * cnt] = -720
-                A[8 * cnt + 3:8 * cnt + 11, 8 * cnt:8 * cnt + 8] = Mat
-            else:
-                A[8 * cnt + 3:8 * cnt + 11, 8 * cnt:8 * cnt + 8] = Mat[:5, :]
+                temp = np.arange(7)
+                vals = factorial(temp)
+                for i, j, k in zip(range(5, 11), reversed(range(9, 15)), range(len(vals))):
+                    A[i + (cnt_up), j + (cnt_up)]  = int(-vals[k+1])
 
-            cnt += 1
+                #Assign the nth order derivative matrix continuity constraints
+                A[cnt_up + 4:cnt_up + 11, cnt_up:cnt_up + 8] = Mat
+            
+            #Exlcuding the last one because the trajectory ends and only the (n-1)th derivative must be continuous everywhere
+            else: 
+                A[cnt_up + 4:cnt_up + 11, cnt_up:cnt_up + 8] = Mat[:4]
 
-        A = A.tocsc()
-        self.C = spsolve(A, B).toarray()
+            #setting the B Matrix Results
+            B[cnt_up + 3: cnt_up + 5] = self.points[cnt:(cnt+2)]
 
+        self.X = spsolve(A, B)
 
+    #7th Order Polym
     def generate_M(self, curr_t):
-        return np.array([[0,                    0,                   0,                  0,                  0,                  0,           0,      1],
+        return np.array([
                          [curr_t ** 7,          curr_t ** 6,         curr_t ** 5,        curr_t ** 4,        curr_t ** 3,        curr_t ** 2, curr_t, 1],
                          [7    * (curr_t ** 6), 6   * (curr_t ** 5), 5 * (curr_t ** 4),  4  * (curr_t ** 3), 3 * (curr_t ** 2),  2 * curr_t,  1,      0],
                          [42   * (curr_t ** 5), 30  * (curr_t ** 4), 20 * (curr_t ** 3), 12 * (curr_t ** 2), 6 * curr_t,         2,           0,      0],
@@ -210,18 +225,13 @@ class WorldTraj(object):
         else:
             for idx in range(self.points_len-1):
                 if t < self.segment_startime[idx+1]:
-                    t = t - self.segment_startime[idx]
-                    t_arr_iter = self.generate_M(t)[1:6,:]
-
-                    resu     = t_arr_iter @ self.C[idx * 8:(idx * 8) + 8, :]
-                    x        = resu[0, :]
-                    x_dot    = resu[1, :]
-                    x_ddot   = resu[2, :]
-                    x_dddot  = resu[3, :]
-                    x_ddddot = resu[4, :]
+                    XX       = self.generate_M(t - self.segment_startime[idx])[:6] @ self.X[idx * 8:(idx * 8) + 8]
+                    x        = XX[0]
+                    x_dot    = XX[1]
+                    x_ddot   = XX[2]
+                    x_dddot  = XX[3]
+                    x_ddddot = XX[4]
                     break
-
-
         flat_output = { 'x':x, 'x_dot':x_dot, 'x_ddot':x_ddot, 'x_dddot':x_dddot, 'x_ddddot':x_ddddot,
                         'yaw':yaw, 'yaw_dot':yaw_dot}
         return flat_output
